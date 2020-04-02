b623b1dbbe7a3da954312170572e603dc68fecc1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-02 16:29:31 -0400

    first

---
+++ src/App.cy-spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+// without global "cy" the bundler throws an error
+// Module Error (from ./node_modules/eslint-loader/dist/cjs.js):
+// Line 8:5:  'cy' is not defined  no-undef
+// probably need to clean up webpack options more
+/* global cy */
+
+// compare to App.test.js
+import React from 'react'
+import App from './App'
+
+describe('App', () => {
+  it('renders learn react link', () => {
+    cy.mount(<App />)
+    cy.contains(/Learn React/)
+  })
+})
