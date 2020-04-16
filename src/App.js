4cb59e3219ee448d6273ee89adf9a1e40453fc74
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-16 10:29:11 -0400

    working static images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-// this SVG will generate an external URL 😢
-import logo from './logo.svg';
 // this CSS will be inlined ✅
 import './App.css';
+import logo from './logo.svg'; // => "/__root/src/logo.svg"
+import cypressLogo from './cypress-logo-dark.png' // => "/__root/src/cypress-logo-dark.png"
+
+// large image will be transformed into
+// a different url static/media/vans.25e5784d.jpg
+// import giantImage from './vans.jpg'
+
+// we cannot load the image from direct url
+// import giantImage from '/__root/src/vans.jpg'
+// <img src={giantImage} alt="cypress-logo" />
 
 function App() {
   return (
     <div className="App">
+      <img src={cypressLogo} className="Cypress-log" />
+
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
