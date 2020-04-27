e5ed18892d69f59d4ce8f7c3113bf316e5407868
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-27 10:26:51 -0400

    use normal preprocessor and support

---
+++ cypress/plugins/index.js
@@ -0,0 +1,6 @@
+module.exports = (on, config) => {
+  require('cypress-react-unit-test/plugins/cra-v3')(on, config)
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config
+}
